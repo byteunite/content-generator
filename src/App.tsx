b1a5da7d4a1a99b0fcbe8f3254c6446239aeea27
Fachri Hawari <fachri.hawari@gmail.com>
2025-09-29 18:05:33 +0700

    feat: add Navbar component and integrate it into the App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router';
 import Home from './components/Home';
 import Templates from './components/Templates';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/templates" element={<Templates />} />
+        <Route element={
+          <div className="container mx-auto px-4 py-6">
+            <Outlet />
+          </div>
+        }>
+          <Route path="/" element={<Home />} />
+          <Route path="/templates" element={<Templates />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
